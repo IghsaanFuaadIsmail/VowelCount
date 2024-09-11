a5358c06179a78de20a02d1c409b21f8c4607b4b
RC_Student_lab <RC_Student_lab@10.0.0.85>
2024-09-11 12:20:34 +0000

    first commit

---
+++ src/main/java/com/mycompany/vowels/Vowels.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vowels;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class Vowels {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
