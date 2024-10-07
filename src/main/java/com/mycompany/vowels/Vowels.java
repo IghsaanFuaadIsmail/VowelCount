9a732d035822844e71d7dc7c6116f570353126e8
RC_Student_lab <RC_Student_lab@10.0.0.85>
2024-10-07 07:52:43 +0000

    function created to check for vowels

--- src/main/java/com/mycompany/vowels/Vowels.java
+++ src/main/java/com/mycompany/vowels/Vowels.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.vowels;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,32 @@ package com.mycompany.vowels;
 public class Vowels {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        String sentence;
+        System.out.println("Enter a sentence");
+        sentence = sc.nextLine();
+        
     }
+     
+    
+
+public static int countVowels(String sentence){
+    
+    sentence = sentence.toLowerCase();
+        
+      String vowels = "aeiou";
+        int count = 0;
+
+        // Iterate through each character of the sentence
+        for (int i = 0; i < sentence.length(); i++) {
+            char currentChar = sentence.charAt(i);
+            
+            // Check if the character is a vowel by checking its index in the vowels string
+            if (vowels.indexOf(currentChar) != -1) {
+                count++;
+            }
+        }
+        return count;
 }
+}
+        
